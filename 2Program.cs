5a14bce44e4e6f843aaed4db2ede395d1be98e5f
HaluDeCassia <haluanedecassia@gmail.com>
2016-09-18 21:51:47 -0300

    Testando cadastro no banco

---
+++ 2Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ConsoleApp.SQLite
+{
+    public class Progra
+    {
+        /*
+        public static void Mai()
+        {
+            using (var db = new Cinema())
+            {
+                db.Categorias.Add(new Categoria { Descricao = "Descricao linda aqui",  });
+                var count = db.SaveChanges();
+                Console.WriteLine("Salvo com sucesso");
+
+                Console.WriteLine();
+                Console.WriteLine("Todas as Descrições:");
+                foreach (var categoria in db.Categorias)
+                {
+                    Console.WriteLine(categoria.CategoriaId + " - " + categoria.Descricao);
+                }
+            }
+        }
+        */
+    }
+}
